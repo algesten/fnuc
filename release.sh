0c29ff737eed886421207a7079efc229f125d26e
Martin Algesten <martin@algesten.se>
2015-01-24 10:41:04 +0530

    release script

--- release.sh
+++ release.sh
@@ -4,6 +4,9 @@ npm run-script compile
 git add lib
 git ci -m 'compile new version'
 npm version patch
+# remove tag again since bower will set it.
+v=`grep version package.json | awk -F '\"' '{print $4}'`
+git tag -d "v${v}"
 bower version patch
 git push origin
 git push origin --tags
