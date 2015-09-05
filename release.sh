8b27030ed00e40e3b538e43205749d6c23948907
Martin Algesten <martin@algesten.se>
2015-09-05 10:40:26 +0200

    better release

--- release.sh
+++ release.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-npm run-script compile
+npm version patch
+npm publish
 git add lib
 git ci -m 'compile new version'
-npm version patch
 # remove tag again since bower will set it.
 v=`grep version package.json | awk -F '\"' '{print $4}'`
 git tag -d "v${v}"
 bower version patch
 git push origin
 git push origin --tags
-npm publish
