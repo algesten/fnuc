5e5da7bee460a4c4e78f6a720540a5403f5a834c
Martin Algesten <martin@algesten.se>
2015-09-20 14:10:44 +0100

    param for release step

--- release.sh
+++ release.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-npm version patch
+step="minor"
+
+npm version "${step}"
 npm publish
 git add lib
 git ci -m 'compile new version'
 # remove tag again since bower will set it.
 v=`grep version package.json | awk -F '\"' '{print $4}'`
 git tag -d "v${v}"
-bower version patch
+bower version "${step}"
 git push origin
 git push origin --tags
